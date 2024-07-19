2bc22704c695cb0627b09c169557856ee75be9a2
Joey Smith <jsmith@webinertia.net>
2024-07-19 02:18:13 -0500

    Adding directory /data/schema/hop_project.sql Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/App/src/Handler/PingHandler.php
+++ src/App/src/Handler/PingHandler.php
@@ -10,11 +10,19 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 use function time;
+use function password_hash;
+
+use const PASSWORD_DEFAULT;
 
 class PingHandler implements RequestHandlerInterface
 {
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        return new JsonResponse(['ack' => time()]);
+        return new JsonResponse(
+            [
+                'ack' => time(),
+                'hash' => password_hash('password', PASSWORD_DEFAULT),
+            ]
+        );
     }
 }
