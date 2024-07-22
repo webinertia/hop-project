14418e99df29e83e503e3f98848ebfe0efcae243
Joey Smith <jsmith@webinertia.net>
2024-07-21 21:42:21 -0500

    committing initial js code to create a new contact. Its working, but too well. It does way too much. needs fixed. Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -22,11 +22,18 @@
     }
     connectSortables();
 
+    // List variables
     let listForm         = document.getElementById('list-form');
-    let submit           = document.getElementById('create-list-modal-submit');
+    let listSubmit       = document.getElementById('create-list-modal-submit');
     const listModal      = new bootstrap.Modal('#create-list-modal');
     let createListButton = document.getElementById('create-list-modal-button');
     let listBoard        = document.getElementById('list-board');
+
+    // Contact variables
+    let contactForm      = document.getElementById('create-contact-form');
+    let contactSubmit    = document.getElementById('create-contact-modal-submit');
+    const contactModal   = new bootstrap.Modal('#create-contact-modal');
+    const createContactButtons = document.querySelectorAll('.create-contact-button');
 
     if (createListButton !== undefined) {
         createListButton.addEventListener('click', function(e) {
@@ -34,7 +41,7 @@
         });
     }
 
-    async function post(request) {
+    async function postList(request) {
         try {
             console.log("Request:", request);
             const response = await fetch(request)
@@ -51,7 +58,7 @@
         }
     }
 
-    submit.addEventListener('click', function(e) {
+    listSubmit.addEventListener('click', function(e) {
         let action   = listForm.getAttribute('action');
         let formData = new FormData(listForm);
         console.log("formData: ",formData);
@@ -62,6 +69,47 @@
             },
             body: formData,
         });
-        post(request);
+        postList(request);
     });
+
+    async function postContact(request, updateTarget) {
+
+        try {
+            const response = await fetch(request)
+            .then(response => response.text())
+            .then((response) => {
+                //console.log("response", response);
+                updateTarget.insertAdjacentHTML("beforeend", response);
+                contactModal.toggle();
+                contactForm.reset();
+                connectSortables();
+            });
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
+
+    function handleNewContact(updateTarget) {
+        contactSubmit.addEventListener('click', function(e) {
+            let action   = contactForm.getAttribute('action');
+            let formData = new FormData(contactForm);
+            const request = new Request(action, {
+                method: "post",
+                headers: {
+                    "X-Requested-With": "XMLHttpRequest"
+                },
+                body: formData,
+            });
+            postContact(request, updateTarget);
+        });
+    }
+
+    createContactButtons.forEach(el => el.addEventListener('click', event => {
+        // el.dataset-cmlistId
+        let updateTarget = document.querySelector('#list_' + el.dataset.cmlistId + ' .card-body .connectedSortable');
+        document.getElementById('contact-list-id').value = el.dataset.cmlistId;
+        contactModal.toggle();
+        //console.log("updateTarget: ", updateTarget);
+        handleNewContact(updateTarget);
+    }));
 })();
